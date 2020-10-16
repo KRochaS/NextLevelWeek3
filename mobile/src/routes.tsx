1f8e46ac23b5da01efef521917b2807823e5a7fe
KRochaS <karinerocha.s@hotmail.com>
2020-10-15 23:03:20 -0300

    NLW#3 - Aula 4

---
+++ mobile/src/routes.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+import { createStackNavigator } from '@react-navigation/stack';
+
+const { Navigator, Screen} = createStackNavigator();
+
+import OrphanageDetails from './pages/OrphanageDetails';
+import OrphanagesMap from './pages/OrphanagesMap';
+
+export default function Routes(){
+    return (
+        <NavigationContainer>
+            <Navigator screenOptions={{
+                headerShown: false
+            }}>
+                <Screen name="OrphanagesMap" component={OrphanagesMap}/>
+                <Screen name="OrphanageDetails" component={OrphanageDetails}/>
+            </Navigator>
+        </NavigationContainer>
+    )
+}
+
